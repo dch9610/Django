94a4597459aa328efe4771db4ec1f79e76dd83ea
“dch9610” <dch9610@naver.com>
2021-02-13 22:11:17 +0900

    user 정보

--- Django_기초/community/user/admin.py
+++ Django_기초/community/user/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import user
 
 # Register your models here.
+
+# user 추가하기
+class userAdmin(admin.ModelAdmin):
+    # 값을 웹 상에 보여주기
+    list_display = ('username', 'password')
+
+admin.site.register(user, userAdmin)
