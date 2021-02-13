47d1d857123491629f3ef09929e7ac88e57ac702
“dch9610” <dch9610@naver.com>
2021-02-13 22:10:58 +0900

    user 정보 입력

--- Django_기초/community/user/models.py
+++ Django_기초/community/user/models.py
@@ -13,9 +13,19 @@ class user(models.Model):
     registerd_dttm = models.DateTimeField(auto_now_add = True,
                                         verbose_name='등록시간')
 
+    # 문자열 변환 함수 (아이디로 나오게함)
+    def __str__(self):
+        return self.username
+
 
     class Meta:
         # 테이블명 지정
         db_table = 'Online_user'
+
+        # 왼쪽 상단 Myaction 부분이 바뀜
+        verbose_name = '유저 사용자'
+
+        # 장고는 모델을 보여줄때 복수형으로 보여줌 (변경)
+        verbose_name_plural = '유저 사용자'
                     
                     
