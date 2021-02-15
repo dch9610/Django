32091c43b48e318c4a08d9db6ff247dfb83fa5a2
“dch9610” <dch9610@naver.com>
2021-02-15 21:29:38 +0900

    로그인 페이지 값 받아오기

--- Django_기초/community/user/views.py
+++ Django_기초/community/user/views.py
@@ -1,7 +1,35 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.contrib.auth.hashers import make_password # 비밀번호 암호화
+from django.contrib.auth.hashers import make_password, check_password # 비밀번호 암호화
 from .models import user
+
+# 로그인 html 연결
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+    
+        res_data = {}
+
+        if  not (username and password):
+            res_data['error'] = "모든 값을 입력해야합니다."
+            
+        else:
+            Puser = user.objects.get(username=username)
+
+            if check_password(password, Puser.password):
+                # 비밀번호가 일치, 로그인 처리
+                # 세션처리
+                # 
+                pass
+            else:
+                res_data['error'] = '비밀번호가 틀렸습니다.'
+            
+        return render(request, 'login.html', res_data)
+
 
 # Create your views here.
 def register(request):
@@ -38,3 +66,4 @@ def register(request):
             Cuser.save()
 
         return render(request, 'register.html', res_data)
+        
