f4924350ff50cffb0ad958b0812c8c11e55d7333
“dch9610” <dch9610@naver.com>
2021-02-15 21:30:30 +0900

    static 폴더 경로 지정

--- Django_기초/community/community/settings.py
+++ Django_기초/community/community/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# static 폴더를 만든후 경로 지정해주기
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
